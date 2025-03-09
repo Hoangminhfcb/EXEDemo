c26d51691843b5c28d9de5ca4d4034cee4eef24b
Hoangminhfcb <78634684+Hoangminhfcb@users.noreply.github.com>
2025-03-09 13:12:17 +0700

    Add .gitignore file

---
+++ CakeShop/Services/CartService.cs
@@ -0,0 +1,34 @@
+﻿namespace CakeShop.Services
+{
+
+    public class CartService
+    {
+        public event Action? OnCartUpdated;
+        public List<string> CartItems { get; private set; } = new();
+        public bool IsCartVisible { get; private set; } = false;
+
+        public void AddToCart(string item)
+        {
+            CartItems.Add(item);
+            IsCartVisible = true; // Khi thêm sản phẩm, giỏ hàng tự mở
+            NotifyCartUpdated();
+        }
+
+        public void ToggleCart()
+        {
+            IsCartVisible = !IsCartVisible; // Nhấn vào icon thì mở/đóng giỏ hàng
+            NotifyCartUpdated();
+        }
+
+        public void CloseCart()
+        {
+            IsCartVisible = false;
+            NotifyCartUpdated();
+        }
+
+        public int GetCartCount() => CartItems.Count;
+
+        private void NotifyCartUpdated() => OnCartUpdated?.Invoke();
+    }
+
+}
